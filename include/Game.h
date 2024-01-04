abea755562a1a416bebffca7891a57f1d02be71c
spykelionel <social.lionel@gmail.com>
2024-01-04 16:16:06 +0100

    create Game.h

---
+++ include/Game.h
@@ -0,0 +1,17 @@
+#ifndef GAME_H
+#define GAME_H
+
+
+class Game
+{
+    public:
+        Game();
+        void printOut();
+        virtual ~Game();
+
+    protected:
+
+    private:
+};
+
+#endif // GAME_H
