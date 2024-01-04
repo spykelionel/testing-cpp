5f1f80a9740985b6d70d50be6fff152f83e9ca67
spykelionel <social.lionel@gmail.com>
2024-01-04 16:15:49 +0100

    Create Game.cpp

---
+++ src/Game.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "Game.h"
+using namespace std;
+Game::Game()
+{
+
+}
+
+Game::~Game()
+{
+    //dtor
+}
+
+
+void Game::printOut(){
+  cout << "Game instance has been created." <<endl;
+}
